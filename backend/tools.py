6734314d892517bf8d4f8bcc1d52144c8b958ec0
Fredrik Nygård Carlsen <me@frecar.no>
2014-05-11 05:47:58 +0200

    add elasticsearch

--- backend/tools.py
+++ backend/tools.py
@@ -1,5 +1,8 @@
+# coding=utf-8
+
 import json
-from models import Post, PostEncoder
+from models import Post, PostEncoder, Newsfeed
+from pyelasticsearch import ElasticSearch
 
 
 def dump_newsfeed(user_id):
@@ -21,6 +24,24 @@ def dump_newsfeed(user_id):
 
     with open("newsfeed.json", "wb") as file:
         file.write(json.dumps(relevant_posts, cls=PostEncoder))
+
+
+def dump_newsfeed_to_elasticsearch(token):
+    es = ElasticSearch('http://localhost:9200/')
+
+    relevant_posts = []
+
+    for element in Newsfeed.newsfeed(token, [], 0, None):
+        if 'from' in element and 'category' in element['from']:
+            continue
+
+        post = Post(element, token)
+        relevant_posts.append(post)
+
+    data = json.dumps(relevant_posts, cls=PostEncoder)
+
+    print data
+
 
 
 def create_nor_list():
@@ -47,4 +68,6 @@ zut
         file.write(json.dumps(list(words)))
 
 
-create_nor_list()
+#create_nor_list()
+
+dump_newsfeed_to_elasticsearch("CAACEdEose0cBAOTCcP8XZAPm2gUo7BRLmU7gdonLZB6ev4x7pDksFiZCu1GS1UajqC4nSURELpNkhOLcMz9aMaZChStp2VpQjjUZB17NQxcoIAsbSUDVn7pEAS7RCHcYOKlzjvluIX4PKriyTqfRLYWPYccPHu9klFZAujioCZBfOKS0FkKOgLRc1tZBf6NvaUbSXVYFoWWMngZDZD")
